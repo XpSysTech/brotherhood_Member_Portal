84a5809030efc5f4b879c6822a020b64bac134b0
XpSys Technology <shakeelhoosain@xpsys.com.na>
2026-04-25 23:29:12 +0200

    Refactor JWT authentication setup in AuthExtensions

--- brotherhood_Member_Portal/Brotherhood_Portal.API/Extensions/AuthExtensions.cs
+++ brotherhood_Member_Portal/Brotherhood_Portal.API/Extensions/AuthExtensions.cs
@@ -1,4 +1,4 @@
-﻿using Brotherhood_Portal.Domain.Entities;
+using Brotherhood_Portal.Domain.Entities;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
@@ -7,78 +7,50 @@ namespace Brotherhood_Portal.API.Extensions;
 
 public static class AuthExtensions
 {
-    public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection AddJwtAuthentication(
+        this IServiceCollection services,
+        IConfiguration configuration)
     {
-        // var jwtSettings = configuration
-        //     .GetSection("Jwt")
-        //     .Get<JwtSettings>()
-        //     ?? throw new Exception("JWT configuration missing");
-
         var jwtSettings = new JwtSettings
         {
             TokenKey = configuration["Jwt:TokenKey"],
             Issuer = configuration["Jwt:Issuer"],
             Audience = configuration["Jwt:Audience"],
-            ExpiryMinutes = int.Parse(configuration["Jwt:ExpiryMinutes"] ?? "15")
+            ExpiryMinutes = int.Parse(
+                configuration["Jwt:ExpiryMinutes"] ?? "15")
         };
 
         if (string.IsNullOrWhiteSpace(jwtSettings.TokenKey))
             throw new Exception("JWT TokenKey is missing");
 
-﻿using Brotherhood_Portal.Domain.Entities;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-
-namespace Brotherhood_Portal.API.Extensions;
-
-public static class AuthExtensions
-{
-    public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
-    {
-        // var jwtSettings = configuration
-        //     .GetSection("Jwt")
-        //     .Get<JwtSettings>()
-        //     ?? throw new Exception("JWT configuration missing");
-
-        var jwtSettings = new JwtSettings
+        services.AddAuthentication(options =>
         {
-            TokenKey = configuration["Jwt:TokenKey"],
-            Issuer = configuration["Jwt:Issuer"],
-            Audience = configuration["Jwt:Audience"],
-            ExpiryMinutes = int.Parse(configuration["Jwt:ExpiryMinutes"] ?? "15")
-        };
+            options.DefaultAuthenticateScheme =
+                JwtBearerDefaults.AuthenticationScheme;
 
-        if (string.IsNullOrWhiteSpace(jwtSettings.TokenKey))
-            throw new Exception("JWT TokenKey is missing");
+            options.DefaultChallengeScheme =
+                JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters =
+                new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey =
+                        new SymmetricSecurityKey(
+                            Encoding.UTF8.GetBytes(jwtSettings.TokenKey)),
 
-	services.AddAuthentication(options =>
-	{
-	    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-	    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-	})
-	.AddJwtBearer(options =>
-	{
-	    options.TokenValidationParameters = new TokenValidationParameters
-	    {
-		ValidateIssuerSigningKey = true,
-		IssuerSigningKey = new SymmetricSecurityKey(
-		    Encoding.UTF8.GetBytes(jwtSettings.TokenKey)),
+                    ValidateIssuer = true,
+                    ValidIssuer = jwtSettings.Issuer,
 
-		ValidateIssuer = true,
-		ValidIssuer = jwtSettings.Issuer,
+                    ValidateAudience = true,
+                    ValidAudience = jwtSettings.Audience,
 
-		ValidateAudience = true,
-		ValidAudience = jwtSettings.Audience,
-
-		ValidateLifetime = true,
-		ClockSkew = TimeSpan.Zero
-	    };
-	});
-
-        return services;
-    }
-}
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
+                };
+        });
 
         return services;
     }
